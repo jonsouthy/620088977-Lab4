0ce2699bc1620e71e47b8f504c7170dc8abe68f8
jonsouthy <32474728+jonsouthy@users.noreply.github.com>
2017-10-20 13:22:10 -0500

    Add files via upload

--- maze.js
+++ maze.js
@@ -1,4 +1,7 @@
+var loss = false;
+
 window.onload = function() {
+    document.getElementById("end").onmouseover = overEnd;
     var boundaries = document.querySelectorAll("div#maze div.boundary");
     for (var x = 0; x < boundaries.length; x++) {
         boundaries[x].onmouseover = overBoundary;
@@ -6,8 +9,25 @@ window.onload = function() {
 };
 
 function overBoundary() {
+    loss = true;
     var boundaries = document.querySelectorAll("div#maze div.boundary");
     for (var x = 0; x < boundaries.length; x++) {
-        boundaries[x].style.backgroundColor = "red";
+        boundaries[x].classList.add("youlose");
+    }
+}
+
+function startClick() {
+    loss = false;
+    var boundaries = document.querySelectorAll("div#maze div.boundary");
+    for (var x = 0; x < boundaries.length; x++) {
+        boundaries[x].classList.remove("youlose");
+    }
+}
+
+function overEnd() {
+    if(loss) {
+       alert ("You lose");
+    } else {
+       alert ("You win");
     }
 }
